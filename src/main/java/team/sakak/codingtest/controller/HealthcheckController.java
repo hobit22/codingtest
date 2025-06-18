66aa83de82216fa9dd37141b7baef847e011276c
hobit22 <hobit22@naver.com>
2025-06-18 12:33:36 +0900

    Feat: HealthcheckController 추가

---
+++ src/main/java/team/sakak/codingtest/controller/HealthcheckController.java
@@ -0,0 +1,13 @@
+package team.sakak.codingtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthcheckController {
+
+    @GetMapping("/health")
+    public String healthcheck() {
+        return "ok";
+    }
+}
