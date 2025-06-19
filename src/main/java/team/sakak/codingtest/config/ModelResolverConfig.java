06e23ef67110154c5a792ab28b8191c4edfa6f3e
hobit22 <hobit22@naver.com>
2025-06-20 08:40:27 +0900

    Feat: response snake case로 수정

---
+++ src/main/java/team/sakak/codingtest/config/ModelResolverConfig.java
@@ -0,0 +1,17 @@
+package team.sakak.codingtest.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ModelResolverConfig {
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
+    }
+}
